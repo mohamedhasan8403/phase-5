2e602e1f7c9c6c1745de49fade78fee55883eeee
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2025-07-19 17:58:09 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 
 # phsase 3 - Code
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 
 # Simulated LIDAR data (360-degree scan, 1-degree resolution)
 def simulate_lidar(position, obstacles, max_range=10):
